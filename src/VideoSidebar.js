79d89e7959d91caeaee534a301dfe867eac8f169
shekhars-realm <sbhattacharya099@gmail.com>
2020-09-30 16:50:47 +0200

    videSidebar.js added

--- src/VideoSidebar.js
+++ src/VideoSidebar.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import './VideoSidebar'
+import React, {useState} from 'react';
+import './VideoSidebar.css'
+import FavoriteIcon from '@material-ui/icons/Favorite'
+import MessageIcon from "@material-ui/icons/Message";
+import ShareIcon from "@material-ui/icons/Share";
 
 const VideoSidebar = () => {
+    const [liked, setLiked] = useState(false)
     return (
-        <div>
-            
+      <div class="videoSidebar">
+        <div class="videoSidebar__button">
+          <FavoriteIcon fontSize="large" />
+          <p>100</p>
         </div>
+        <div class="videoSidebar__button">
+          <MessageIcon fontSize="large" />
+          <p>250</p>
+        </div>
+        <div class="videoSidebar__button">
+          <ShareIcon fontSize="large" />
+          <p>30</p>
+        </div>
+      </div>
     );
 };
 
